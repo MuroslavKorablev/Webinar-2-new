f6b2878b0bf85be7555a0f2155271ee45eb6f64b
Kleo <mmttdd16@gmail.com>
2023-09-03 20:48:22 +0300

    Finished Ex1

--- Ex1/Program.cs
+++ Ex1/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int[] array = {1,2,3,4,5,6,7,8,9,10,4,11,12,13,14,15};
+
+int size = array.Length;
+int find = 4;
+
+int index = 0;
+
+while (index < size)
+{
+    if (array[index] == find)
+    {
+        System.Console.WriteLine(index);
+        break;
+    }
+    index++;
+}
